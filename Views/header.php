f47de830b86b8b2e54a901e6824f86f4a2c0cd4b
Juan Ormelli <juan_ormelli@hotmail.com>
2020-09-29 23:27:49 -0300

    Refatoracao-pastas

--- Views/header.php
+++ Views/header.php
@@ -1,6 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/P.I/public/styles/header.css">
+    <title>Document</title>
+</head>
 <body>
-<header>
+      <header>
         <div class="landing-page">
             <li class="button-list">
                 <button class="button" name="Inicio" onclick="window.location.href=''">Inicio</button>
@@ -13,3 +21,5 @@
 
     </header>
 </body>
+</html>
+
--- Views/index.php
+++ Views/index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/P.I/Models/index-page.css">
-    <link rel="stylesheet" href="/P.I/Models/header.css">
+    <link rel="stylesheet" href="/P.I/public/styles/index-page.css">
     <?php include_once "header.php"?>
     <title>Inicio</title>
 </head>
@@ -15,13 +14,13 @@
         <li class="menu-content">
             <button class="menu-button" nome= "menu-button" onclick="window.location.href=' login.html'">
                 <div class="menu">
-                    <img src="/P.I/public/cliente.jpg" alt="Clientes">
+                    <img src="/P.I/public/img/cliente.jpg" alt="Clientes">
                     <h1 class="menu-title">Clientes</h1>
                 </div>
             </button>
             <button class="menu-button" nome= "menu-button" onclick="window.location.href=' login.html'">
                 <div class="menu">
-                    <img src="/P.I/public/PEDIDO.png" alt="Produtos">
+                    <img src="/P.I/public/img/PEDIDO.png" alt="Produtos">
                     <h1 class="menu-title">Produtos</h1>
                 </div>
             </button>
@@ -30,13 +29,13 @@
         <li class="menu-content">
             <button class="menu-button" nome= "menu-button" onclick="window.location.href=' login.html'">
                 <div class="menu">
-                    <img src="/P.I/public/Cadastro-Cliente.png" alt="Cadastrar/Alterar-dados-de-cliente">
+                    <img src="/P.I/public/img/Cadastro-Cliente.png" alt="Cadastrar/Alterar-dados-de-cliente">
                     <h1 class="menu-title">Cadastrar/Alterar dados de cliente</h1>
                 </div>
             </button>
             <button class="menu-button" nome= "menu-button-cadastrar" onclick="window.location.href=' login.html'">
                 <div class="menu">
-                        <img class="adjust"src="/P.I/public/Adicionar-produtos.png" alt="Cadastro-de-produto">
+                        <img class="adjust"src="/P.I/public/img/Adicionar-produtos.png" alt="Cadastro-de-produto">
                     <h1 class="menu-title">Cadastrar Produtos</h1>      
                 </div>
             </button>
