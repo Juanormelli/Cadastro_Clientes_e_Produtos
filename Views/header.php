311f587b79e44c65a25697d3b7866228d364e1ef
Juan Ormelli <juan_ormelli@hotmail.com>
2020-09-29 23:11:50 -0300

    Refatoracao-Header

---
+++ Views/header.php
@@ -0,0 +1,15 @@
+
+<body>
+<header>
+        <div class="landing-page">
+            <li class="button-list">
+                <button class="button" name="Inicio" onclick="window.location.href=''">Inicio</button>
+                <button class="button" name="clientes" onclick="window.location.href=''">Clientes</button>
+                <button class="button" name="produtos" onclick="">Produtos</button>
+                <button class="button" name="cadastrar-clientes" onclick="">Cadastrar Cliente</button>
+                <button class="button" name="cadastrar-pedidos" onclick="">Cadastrar Produtos</button>
+            </li>
+        </div>
+
+    </header>
+</body>
--- Views/index.php
+++ Views/index.php
@@ -5,22 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/P.I/Models/index-page.css">
     <link rel="stylesheet" href="/P.I/Models/header.css">
+    <?php include_once "header.php"?>
     <title>Inicio</title>
 </head>
 
 <body>
-    <header>
-        <div class="landing-page">
-            <li class="button-list">
-                <button class="button" name="Inicio" onclick="window.location.href=''">Inicio</button>
-                <button class="button" name="clientes" onclick="window.location.href=''">Clientes</button>
-                <button class="button" name="produtos" onclick="">Produtos</button>
-                <button class="button" name="cadastrar-clientes" onclick="">Cadastrar Cliente</button>
-                <button class="button" name="cadastrar-pedidos" onclick="">Cadastrar Produtos</button>
-            </li>
-        </div>
-
-    </header>
+   
     <div id="principal-page">
         <li class="menu-content">
             <button class="menu-button" nome= "menu-button" onclick="window.location.href=' login.html'">
